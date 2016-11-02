2fa2e0475c7efa1f34eb9a2f0f0ea97a6f6d39eb
Ellie Dori & Kimberly Pena & Ali Baker <pair+elliedori.kimmyp23.thealicat13@devbootcamp.com>
2016-11-01 17:19:41 -0700

    Add create and show views for bars

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,16 +1,28 @@
 class BarsController < ApplicationController
+  before_action :find_neighborhood, only: [:index, :new, :create, :show]
+  before_action :find_bar, except: [:index, :new, :create]
+
   def index
-    @neighborhood = Neighborhood.find(params[:neighborhood_id])
     @bars = @neighborhood.bars
   end
 
   def show
+
   end
 
   def create
+    @bar = @neighborhood.bars.new(bar_params)
+    if @bar.save
+      redirect_to neighborhood_bar_path(@neighborhood, @bar)
+    else
+      @errors = @bar.errors.full_messages
+      render 'new'
+    end
   end
 
   def new
+    # @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    @bar = Bar.new
   end
 
   def edit
@@ -21,4 +33,20 @@ class BarsController < ApplicationController
 
   def destroy
   end
+
+
+  private
+
+  def find_neighborhood
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+  end
+
+  def find_bar
+    @bar = Bar.find(params[:id])
+  end
+
+  def bar_params
+    params.require(:bar).permit(:name, :specialty_drink, :cash_only)
+  end
+
 end
--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,3 +1,6 @@
 class Bar < ApplicationRecord
   belongs_to :neighborhood
+
+  validates :name, presence: true
+  validates :specialty_drink, presence: true
 end
